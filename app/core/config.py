c94c72a8fe90488bf3033951f9920250793cf77c
Simon <simon@fa-loeffler.de>
2025-07-02 23:03:10 +0200

    enviroment Variabeln als Config

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+# falls eine .env Datei existiert, werden die Umgebungsvariablen geladen
+load_dotenv()
 
 class DbSettings:
-    host = "192.168.188.10"
-    port = 3306
-    user = "root"
-    password = "SimonRoot"
-    database = "skiservice_dev"
+    host = os.getenv("DB_HOST", "localhost")
+    port = int(os.getenv("DB_PORT", 3306))
+    user = os.getenv("DB_USER", "root")
+    password =  os.getenv("DB_PASSWORD", "password")
+    database = os.getenv("DB_NAME", "skiservice_dev")
     mysql_constring = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 
